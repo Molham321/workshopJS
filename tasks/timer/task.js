5c49cc024ecf8cb1a46a231efd6fdbd267d7e8d3
Molham Al-khodari <molhamalkhodari@gmail.com>
2022-09-07 14:55:58 +0200

    push

--- tasks/timer/task.js
+++ tasks/timer/task.js
@@ -1,43 +1,45 @@
 ﻿let questions = [
     {
-        'id': 1,
-        'text': '1 + 1 = ?'
+        prompt: "1 + 1 = ?\n(a) 1\n(b) 2\n(c) 3\n(d) 4",
+        answer: "a"
     },
     {
-        'id': 2,
-        'text': '2 + 2 = ?'
+        prompt: "2 + 2 = ?\n(a) 1\n(b) 4\n(c) 3\n(d) 4",
+        answer: "b"
+    },    {
+        prompt: "3 + 3 = ?\n(a) 1\n(b) 2\n(c) 6\n(d) 4",
+        answer: "c"
+    },    {
+        prompt: "4 + 4 = ?\n(a) 1\n(b) 2\n(c) 3\n(d) 8",
+        answer: "d"
+    },    {
+        prompt: "5 + 5 = ?\n(a) 10\n(b) 2\n(c) 3\n(d) 4",
+        answer: "a"
     },
-    {
-        'id': 3,
-        'text': '3 + 3 = ?'
-    },
-    {
-        'id': 4,
-        'text': '4 + 4 = ?'
-    },
-    {
-        'id': 5,
-        'text': '5 + 5 = ?'
-    },
-    {
-        'id': 6,
-        'text': '6 + 6 = ?'
-    },
-    {
-        'id': 7,
-        'text': '7 + 7 = ?'
-    },
-    {
-        'id': 8,
-        'text': '8 + 8 = ?'
-    },
-    {
-        'id': 9,
-        'text': '9 + 9 = ?'
-    },
-    {
-        'id': 10,
-        'text': '10 + 10 = ?'
-    },
-
 ]
+
+let score = 0;
+let time = 0;
+
+document.getElementById('score').innerText = score;
+document.getElementById('timer').innerText = time;
+
+document.getElementById('mission').innerHTML = questions[0]
+
+
+// for (let i = 0; i < questions.length; i++) {
+
+
+
+//     let mission = document.getElementById('mission')
+//     mission.innerText = questions[i].prompt;
+
+//     if(mission == questions[i].answer) {
+//         score++;
+//         alert("Correct!");
+//     } else {
+//         alert("WRONG!");
+//     }
+
+//     score = document.getElementById('score')
+// }
