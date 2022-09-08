11554dc594be53fd9aa4abf6619e617276ba2a08
Molham Al-khodari <molhamalkhodari@gmail.com>
2022-09-08 15:00:47 +0200

    push

--- tasks/memory/task.js
+++ tasks/memory/task.js
@@ -1,33 +1,81 @@
-﻿let pokeArray = [];
+﻿
+const photosElement = document.querySelectorAll('.photo');
+const counterElemnt = document.getElementById('counter');
 
-function fetchPokemonData(pokemon) {
-    let url = pokemon.url;
-    return fetch(url)
-        .then(response => response.json());
-}
+let shuffledPictures, currentPictureIndex
 
-function fetchKantoPokemon() {
-    return fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-        .then(response => response.json())
-        .then(function (allpokemon) {
-            return Promise.all(allpokemon.results.map(fetchPokemonData))
-                .then(function (pokemons) {
-                    return pokemons.map(function (pokemon) {
-                        return {
-                            name: pokemon.name,
-                            sprite: pokemon.sprites.front_default,
-                            types: pokemon.types.map(type => type.type.name)
-                        };
-                    });
-                });
-        });
-}
 
-if (localStorage.getItem("Pokemon") === null) {
-    fetchKantoPokemon().then(function(pokemons) {
-        pokeArray = pokemons;
-        localStorage.setItem("pokemon", JSON.stringify(pokemons));
+
+window.addEventListener('load', startGame);
+
+let counterValue;
+counterValue = parseInt(counterElemnt.innerText);
+
+photosElement.forEach(photo => {
+    photo.addEventListener('click', function(e) {
+
+        counterValue++
+        counterElemnt.innerText = counterValue
+
     });
-} else {
-    pokeArray = JSON.parse(localStorage.getItem("Pokemon"));
+})
+
+function startGame () {
+
+    shuffledPictures = pictures.sort(() => Math.random() - .5)
+    currentPictureIndex = 0
+
+    setPic(shuffledPictures[currentPictureIndex])
 }
+
+function setPic(pic) {
+    pic.url.forEach(url => {
+        const div = document.createElement('div')
+        div.classList.add('photo')
+        div.style.backgroundImage = url
+    })
+}
+
+
+const pictures = [
+    {
+        name: 'Apfel',
+        url: './photos/Apfel.jpg'
+    },
+    {
+        name: 'Apfel',
+        url: './photos/Apfel.jpg'
+    },
+    {
+        name: 'Apfel',
+        url: './photos/Apfel.jpg'
+    },
+    {
+        name: 'Apfel',
+        url: './photos/Apfel.jpg'
+    },
+    {
+        name: 'Apfel',
+        url: './photos/Apfel.jpg'
+    },
+    {
+        name: 'Apfel',
+        url: './photos/Apfel.jpg'
+    },
+    {
+        name: 'Apfel',
+        url: './photos/Apfel.jpg'
+    },
+    {
+        name: 'Apfel',
+        url: './photos/Apfel.jpg'
+    },
+    {
+        name: 'Apfel',
+        url: './photos/Apfel.jpg'
+    },
+    {
+        name: 'Apfel',
+        url: './photos/Apfel.jpg'
+    }
+]
--- tasks/timer/task.js
+++ tasks/timer/task.js
@@ -1,45 +1,200 @@
-﻿let questions = [
+﻿
+const task = document.querySelector('.task')
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const timerElement = document.getElementById('timer')
+const scoreElement = document.getElementById('score')
+
+let shuffledQuestions, currentQuestionIndex
+let timerValue, scoreValue
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    if(timerValue > 0) {
+        currentQuestionIndex++
+        setNextQuestion()
+    } else {
+        questionContainerElement.classList.add('hide')
+        nextButton.classList.add('hide')
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+
+})
+
+function startGame() {
+  timerElement.innerText = '30'
+  timerValue = parseInt(timerElement.innerText);
+
+  scoreElement.innerText = '0'
+  scoreValue = parseInt(scoreElement.innerText);
+
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(task)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+
+  if(correct) {
+    timerValue += 5 
+    timerElement.innerText = timerValue
+    scoreValue += 10 
+    scoreElement.innerText = scoreValue
+  } else {
+    timerValue -= 5 
+    timerElement.innerText = timerValue
+  }
+
+  setStatusClass(task, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
     {
-        prompt: "1 + 1 = ?\n(a) 1\n(b) 2\n(c) 3\n(d) 4",
-        answer: "a"
+        question: "What is 11 + 12 + 13 = ?",
+        answers: [
+            {text: '35', correct: false},
+            {text: '36', correct: true},
+            {text: '37', correct: false},
+            {text: '33', correct: false}
+        ]
     },
     {
-        prompt: "2 + 2 = ?\n(a) 1\n(b) 4\n(c) 3\n(d) 4",
-        answer: "b"
-    },    {
-        prompt: "3 + 3 = ?\n(a) 1\n(b) 2\n(c) 6\n(d) 4",
-        answer: "c"
-    },    {
-        prompt: "4 + 4 = ?\n(a) 1\n(b) 2\n(c) 3\n(d) 8",
-        answer: "d"
-    },    {
-        prompt: "5 + 5 = ?\n(a) 10\n(b) 2\n(c) 3\n(d) 4",
-        answer: "a"
+        question: "What is 17 * 2 + 49 = ?",
+        answers: [
+            {text: '83', correct: true},
+            {text: '82', correct: false},
+            {text: '81', correct: false},
+            {text: '80', correct: false}
+        ]
     },
+    {
+        question: "What is 347 + 398 = ?",
+        answers: [
+            {text: '640', correct: false},
+            {text: '740', correct: false},
+            {text: '745', correct: true},
+            {text: '645', correct: false}
+        ]
+    },
+    {
+        question: "What is 444 / 3 = ?",
+        answers: [
+            {text: '144', correct: false},
+            {text: '148', correct: true},
+            {text: '142', correct: false},
+            {text: '143', correct: false}
+        ]
+    },
+    {
+        question: "What is 58 + 9 = ?",
+        answers: [
+            {text: '69', correct: false},
+            {text: '68', correct: false},
+            {text: '67', correct: true},
+            {text: '66', correct: false}
+        ]
+    },
+    {
+        question: "What is 8 * 12 / 8 = ?",
+        answers: [
+            {text: '8', correct: false},
+            {text: '12', correct: true},
+            {text: '21', correct: false},
+            {text: '80', correct: false}
+        ]
+    },
+    {
+        question: "What is 7 + 7 = ?",
+        answers: [
+            {text: '15', correct: false},
+            {text: '16', correct: false},
+            {text: '14', correct: true},
+            {text: '13', correct: false}
+        ]
+    },
+    {
+        question: "What is 849 + 894 + 257 = ?",
+        answers: [
+            {text: '5000', correct: false},
+            {text: '4000', correct: false},
+            {text: '3000', correct: false},
+            {text: '2000', correct: true}
+        ]
+    },
+    {
+        question: "What is 9 + 9 = ?",
+        answers: [
+            {text: '15', correct: false},
+            {text: '16', correct: false},
+            {text: '17', correct: false},
+            {text: '18', correct: true}
+        ]
+    },
+    {
+        question: "What is 10 + 10 / 10 = ?",
+        answers: [
+            {text: '20', correct: false},
+            {text: '11', correct: true},
+            {text: '2', correct: false},
+            {text: '21', correct: false}
+        ]
+    }
 ]
-
-let score = 0;
-let time = 0;
-
-document.getElementById('score').innerText = score;
-document.getElementById('timer').innerText = time;
-
-document.getElementById('mission').innerHTML = questions[0]
-
-
-// for (let i = 0; i < questions.length; i++) {
-
-
-
-//     let mission = document.getElementById('mission')
-//     mission.innerText = questions[i].prompt;
-
-//     if(mission == questions[i].answer) {
-//         score++;
-//         alert("Correct!");
-//     } else {
-//         alert("WRONG!");
-//     }
-
-//     score = document.getElementById('score')
-// }
